194a624801b2accd83650cd97a6d8e62b0ed91d2
Alex Huzo <sanduguzo1337@gmail.com>
2022-09-19 00:49:25 +0300

    Four

---
+++ Four/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lessons
+{
+    internal class Program
+    {
+
+        static string DoubleString(string str)
+        {
+            int a;
+            Console.Write("Введите количество повторений символа : ");
+            a = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.Write(str);
+            }
+
+            return str;
+        }
+
+        static void Main(string[] args)
+
+        {
+            string b;
+
+            Console.WriteLine("Ведите какой символ хотите повторить");
+            string str;
+            str = Console.ReadLine();
+            b = DoubleString(str);
+        }
+    }
+}
+
